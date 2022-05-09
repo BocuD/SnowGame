5a0088476b534b89e80d741d572a9ce0a1869cec
BocuD <duco.born@gmail.com>
2022-05-09 03:46:44 +0200

    Rewrite tilemap to use tris instead of quads

--- SnowGame/TileMap.cpp
+++ SnowGame/TileMap.cpp
@@ -1,11 +1,13 @@
 ﻿#include "TileMap.h"
+#include <SFML/Graphics/Texture.hpp>
 
 bool TileMap::load(sf::Texture* tileset, sf::Vector2u tileSize, std::vector<ldtk::Tile> tiles, unsigned width, unsigned height)
 {
 	texture = tileset;
 	// resize the vertex array to fit the level size
-	vertices.setPrimitiveType(sf::Quads);
-	vertices.resize(width * height * 4);
+	vertices.setPrimitiveType(sf::Triangles);
+
+	vertices.resize(width * height * 6);
 
 	// populate the vertex array, with one quad per tile
 	for (unsigned i = 0; i < width; ++i) 
@@ -25,54 +27,63 @@ bool TileMap::load(sf::Texture* tileset, sf::Vector2u tileSize, std::vector<ldtk
 			const float tv = tileNumber / (tileset->getSize().x / tileSize.x);
 
 			// get a pointer to the current tile's quad
-			sf::Vertex* quad = &vertices[(i + j * width) * 4];
+			sf::Vertex* tris = &vertices[(i + j * width) * 6];
 
-			// define its 4 corners
-			quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-			quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-			quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-			quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+			// define corners
+			tris[0].position = { (float) i * tileSize.x, (float) j * tileSize.y };
+			tris[1].position = { (float) (i + 1) * tileSize.x, (float) j * tileSize.y };
+			tris[2].position = { (float) (i + 1) * tileSize.x, (float) (j + 1) * tileSize.y };
 
-			// define its 4 texture coordinates
-			quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y) + sf::Vector2f(0.5f, 0.5f);
-			quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y) + sf::Vector2f(-0.5f, 0.5f);
-			quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y) + sf::Vector2f(-0.5f, -0.5f);
-			quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y) + sf::Vector2f(0.5f, -0.5f);
+			tris[3].position = tris[0].position;
+			tris[4].position = { (float) i * tileSize.x, (float)(j + 1) * tileSize.y };
+			tris[5].position = tris[2].position;
+
+			//define base texture coordinates
+			tris[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y) + sf::Vector2f(0.5f, 0.5f);
+			tris[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y) + sf::Vector2f(-0.5f, 0.5f);
+			tris[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y) + sf::Vector2f(-0.5f, -0.5f);
+			tris[3].texCoords = tris[0].texCoords;
+			tris[4].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y) + sf::Vector2f(0.5f, -0.5f);
+			tris[5].texCoords = tris[2].texCoords;
 
 #pragma warning(pop)
-
+			//see drawing for context (project root / texture coords.png)
 			if (tile.flipX)
 			{
-				sf::Vector2f temp = quad[0].texCoords;
-				quad[0].texCoords = quad[1].texCoords;
-				quad[1].texCoords = temp;
-				temp = quad[2].texCoords;
-				quad[2].texCoords = quad[3].texCoords;
-				quad[3].texCoords = temp;
+				tris[0].texCoords = tris[1].texCoords;
+				tris[1].texCoords = tris[3].texCoords;
+				tris[3].texCoords = tris[0].texCoords;
+
+				tris[2].texCoords = tris[4].texCoords;
+				tris[4].texCoords = tris[5].texCoords;
+				tris[5].texCoords = tris[2].texCoords;
 			}
 
 			if (tile.flipY)
 			{
-				sf::Vector2f temp = quad[0].texCoords;
-				quad[0].texCoords = quad[3].texCoords;
-				quad[3].texCoords = temp;
-				temp = quad[1].texCoords;
-				quad[1].texCoords = quad[2].texCoords;
-				quad[2].texCoords = temp;
+				tris[0] = tris[4];
+				tris[4] = tris[3];
+				tris[3] = tris[0];
+
+				tris[5] = tris[1];
+				tris[1] = tris[2];
+				tris[2] = tris[5];
 			}
 		}
 	}
 	return true;
 }
 
-void TileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
+void TileMap::draw(sf::RenderTarget& target, const sf::RenderStates& states) const
 {
+	sf::RenderStates statesCopy(states);
+
 	// apply the transform
-	states.transform *= getTransform();
+	statesCopy.transform *= getTransform();
 
 	// apply the tileset texture
-	states.texture = texture;
+	statesCopy.texture = texture;
 
 	// draw the vertex array
-	target.draw(vertices, states);
+	target.draw(vertices, statesCopy);
 }
--- SnowGame/TileMap.h
+++ SnowGame/TileMap.h
@@ -1,11 +1,8 @@
 ﻿#pragma once
-#include <string>
 #include <LDtkLoader/Tile.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
-#include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Transformable.hpp>
-#include <SFML/Graphics/Vertex.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
 
 namespace ldtk
@@ -19,7 +16,7 @@ public:
     bool load(sf::Texture* tileset, sf::Vector2u tileSize, std::vector<ldtk::Tile> tiles, unsigned width, unsigned height);
 
 private:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override;
 
     sf::Texture* texture;
     sf::VertexArray vertices;
