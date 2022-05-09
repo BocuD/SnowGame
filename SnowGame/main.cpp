021218095d7065962635e689395160d17725f826
BocuD <duco.born@gmail.com>
2022-05-09 03:48:02 +0200

    Don't dynamically allocate memory for Game and Window

--- SnowGame/main.cpp
+++ SnowGame/main.cpp
@@ -4,16 +4,15 @@
 #include "Scene.h"
 #include "Game.h"
 
-Game* game;
 const float dt = 1/60.f;
 
 int main(char* args)
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Snow Game");
+    sf::RenderWindow window(sf::VideoMode(1280, 800), "Snow Game");
     window.setVerticalSyncEnabled(true);
 
-    game = new Game;
-    game->init();
+    Game game;
+    game.init();
 
     sf::Clock frameClock;
     sf::Clock fixedClock;
@@ -23,13 +22,13 @@ int main(char* args)
 
     float fixedUpdateTimer = 0.f;
 
-    while (window.isOpen()) 
+    while (window.isOpen())
     {
         float updateTime = frameClock.getElapsedTime().asSeconds();
         frameClock.restart();
 
         sf::Event e;
-        while (window.pollEvent(e)) 
+        while (window.pollEvent(e))
         {
             switch(e.type)
             {
@@ -39,7 +38,7 @@ int main(char* args)
                 return 0;
 
             default:
-                game->eventHandler(e, &window);
+                game.eventHandler(e, &window);
                 break;
             }
         }
@@ -49,21 +48,22 @@ int main(char* args)
 
         while(fixedUpdateTimer >= dt)
         {
-            game->fixedUpdate();
+            game.fixedUpdate();
             fixedUpdateTimer -= dt;
         }
 
         window.clear();
 
-        game->update(updateTime);
-        game->draw(&window);
+        game.update(updateTime);
+        game.draw(&window);
 
         //draw fps counter in window title
-        std::string windowTitle = "Snow Game -- FPS: " + 
+        std::string windowTitle = "Snowball Jump -- FPS: " + 
             std::to_string(1 / updateTime).substr(0, 5);
         window.setTitle(windowTitle);
 
         window.display();
     }
+
     return 0;
 }
